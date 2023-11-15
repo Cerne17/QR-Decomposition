232026672ec0c6a2ebbd797bc4968d5fea916762
unknown <miguelbadanycerne.@poli.ufrj.br>
2023-11-15 17:55:04 -0300

    QR non permutation done, reversed OOP

--- src/householder.py
+++ src/householder.py
@@ -1,49 +1,39 @@
 import numpy as np
 import numpy.linalg as linalg
 
-class Householder:
-    def __init__(self, matrixA):
-        self.matrixA = matrixA
-        self.lineNumber = len(matrixA)
-        self.incompleteHouseholderMatrix = []
-        self.completeHouseholderMatrix = []
+def getHouseholderMatrix(matrixA):
+    A1 = []
+    lineNumber = len(matrixA)
+    for line in range(lineNumber):
+        A1.append(matrixA[line, 0])
 
-    def getHouseholderMatrix(self):
-        A1 = []
-        for line in range(self.lineNumber):
-            A1.append(self.matrixA[line, 0])
+    arrayA1 = np.array(A1)
+    normA1 = linalg.norm(arrayA1)
 
-        arrayA1 = np.array(A1)
-        normA1 = linalg.norm(arrayA1)
+    base_vector = np.array([[1]] + [[0]] * (lineNumber - 1))
 
-        base_vector = np.array([[1]] + [[0]] * (self.lineNumber - 1))
+    if arrayA1[0] < 0:
+        vectorV = arrayA1 - normA1 * base_vector
+    else:
+        vectorV = arrayA1 + normA1 * base_vector
 
-        if arrayA1[0] < 0:
-            vectorV = arrayA1 - normA1 * base_vector
-        else:
-            vectorV = arrayA1 + normA1 * base_vector
+    if np.linalg.norm(vectorV) == 0:
+        vectorU = vectorV
+    else:
+        vectorU = vectorV / np.linalg.norm(vectorV)
 
-        if np.linalg.norm(vectorV) == 0:
-            vectorU = vectorV
-        else:
-            vectorU = vectorV / np.linalg.norm(vectorV)
+    matricesMultiplication = vectorU @ vectorU.transpose()
+    identity = np.identity(len(matricesMultiplication))
+    incompleteHouseholderMatrix = identity - 2 * matricesMultiplication
 
-        matricesMultiplication = vectorU @ vectorU.transpose()
-        identity = np.identity(len(matricesMultiplication))
-        householderMatrix = identity - 2 * matricesMultiplication
+    return incompleteHouseholderMatrix  
 
-        self.incompleteHouseholderMatrix = householderMatrix
+def completeHouseholderMatrix(incompleteMatrix, size):
+    householderMatrix = np.identity(size)
 
-        return householderMatrix
+    incompleteMatrixSize = len(incompleteMatrix)
 
-    def completeHouseholderMatrix(self, size):
-        householderMatrix = np.identity(size)
+    startPoint = size - incompleteMatrixSize
+    householderMatrix[startPoint:, startPoint:] = incompleteMatrix
 
-        incompleteMatrixSize = len(self.incompleteHouseholderMatrix)
-
-        startPoint = size - incompleteMatrixSize
-        householderMatrix[startPoint:, startPoint:] = self.incompleteHouseholderMatrix
-
-        self.completehouseholdermatrix = np.array(householderMatrix)
-
-        return householderMatrix
+    return householderMatrix
--- src/qrdecomposition.py
+++ src/qrdecomposition.py
@@ -1,48 +1,52 @@
 import numpy as np
-from householder import Householder
+from householder import getHouseholderMatrix, completeHouseholderMatrix
 from matricesgeneration import RandomMatrixGenerator
 
-class QRDecomposition:
-    def __init__(self, matrixA):
-        self.matrixA      = matrixA
-        self.matrixQ      = Householder(matrixA).getHouseholderMatrix().transpose()
-        self.matrixR      = Householder(matrixA).getHouseholderMatrix() @ matrixA
-        self.lineNumbersA = len(matrixA)
+def qrDecompositionNonPermutation(matrixA):
+    matrixQ      = getHouseholderMatrix(matrixA).transpose()
+    matrixR      = getHouseholderMatrix(matrixA) @ matrixA
+    lineNumbersA = len(matrixA)
 
-    def qrDecompositionNonPermutation(self):
-        minimumDimension = min(len(self.matrixA), len(self.matrixA[0]))
+    minimumDimension = min(len(matrixA), len(matrixA[0]))
 
-        for i in range(1, minimumDimension):
-            auxiliarMatrix = np.array(self.matrixR[i:, i:])
-            householder = Householder(auxiliarMatrix)
-            incompleteHouseholderMatrix = householder.getHouseholderMatrix()
-            householder.incompleteHouseholderMatrix = incompleteHouseholderMatrix
+    for i in range(1, minimumDimension):
+        auxiliarMatrix = np.array(matrixR[i:, i:])
+        incompleteHouseholderMatrix = getHouseholderMatrix(auxiliarMatrix)
 
-            completeHouseholderMatrix = householder.completeHouseholderMatrix(self.lineNumbersA)
+        householderMatrix = completeHouseholderMatrix(incompleteHouseholderMatrix,lineNumbersA)
 
-            self.matrixR = completeHouseholderMatrix @ self.matrixR
-            self.matrixQ = self.matrixQ @ completeHouseholderMatrix.transpose()
+        matrixR = householderMatrix @ matrixR
+        matrixQ = matrixQ @ householderMatrix.transpose()
 
-        return self.matrixQ, self.matrixR
-
+    return matrixQ, matrixR
 
 matrices33Complete = RandomMatrixGenerator(3, 3, 3)
 matrices55Complete = RandomMatrixGenerator(5, 5, 5)
 matrixA = matrices33Complete.generateRandomMatrix()
-matrixB = matrices55Complete.generateRandomMatrix()
 
 print("Matrix A:")
 print(matrixA)
 print()
 
-print("Matrix B:")
-print(matrixB)
-print()
-
-qrSolutionA = QRDecomposition(matrixA).qrDecompositionNonPermutation()
+qrSolutionA = qrDecompositionNonPermutation(matrixA)
 
 print("Matrix Q:")
 print(qrSolutionA[0])
 print()
 print("Matrix R:")
 print(qrSolutionA[1])
+
+matrices34 = RandomMatrixGenerator(4, 4, 4)
+matrixB = matrices34.generateRandomMatrix()
+
+print("Matrix A:")
+print(matrixB)
+print()
+
+qrSolutionB = qrDecompositionNonPermutation(matrixB)
+
+print("Matrix Q:")
+print(qrSolutionB[0])
+print()
+print("Matrix R:")
+print(qrSolutionB[1])
